249117862a4ba5824920f8708d6fa171fe00850d
japarajuli <108414466+japarajuli@users.noreply.github.com>
2022-06-30 07:59:00 -0400

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score,classification_report,precision_score,recall_score
+#from imblearn import under_sampling, over_sampling
+#from imblearn.over_sampling import SMOTE
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
